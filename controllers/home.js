b5db1236edc617991e68967fb40e2082a874b92a
Justin Quinn <jpq3366@gmail.com>
2023-01-10 21:58:36 -0500

    Configured models, views, controllers, and routes for projects

---
+++ controllers/home.js
@@ -0,0 +1,26 @@
+const itemList = require("../models/itemlist");
+
+module.exports = {
+  getIndex: async (req, res) => {
+    try {
+      const items = await itemList.find();
+      res.render("index.ejs", { itemList: items });
+    } catch (err) {
+      if (err) return res.status(500).send(err);
+    }
+  },
+  createItem: async (req, res) => {
+    const newItem = new ItemList({
+      textInput: req.body.textInput,
+      numInput: req.body.numInput,
+    });
+    try {
+      await newItem.save();
+      console.log(newItem);
+      res.redirect("/");
+    } catch (err) {
+      if (err) return res.status(500).send(err);
+      res.redirect("/");
+    }
+  },
+};
--- server.js
+++ server.js
@@ -1,11 +1,24 @@
 //todo - Declare Variables
+const express = require("express");
+const app = express();
+const PORT = 8000;
+const mongoose = require("mongoose");
+require("dotenv").config({ path: "./config/.env" });
 
 //todo - Import functions/routes
+const connectDB = require("./config/database");
+const homeRoutes = require("./routes/home");
 
 // todo - Connect to Database
+connectDB();
 
 //todo - Set Middleware
+app.set("view engine", "ejs");
+app.set(express.static("public"));
+app.use(express.urlencoded({ extended: true }));
 
 //todo - Set Routes
+app.use("/", homeRoutes);
 
 //todo - Start Server
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
